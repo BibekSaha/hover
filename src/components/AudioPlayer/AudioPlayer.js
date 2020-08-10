7afb920f3e62977d109fe3e8affd4178f70f2a2b
Bibek Saha <thebibeksaha@gmail.com>
2020-08-11 04:36:47 +0530

    made the media player working

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player/youtube';
+import '../Loader/Loader';
 import './AudioPlayer.css';
 
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
-    this.videoId = this.props.audioPreviewURL.split('v=')[1];
+    this.audioRef = React.createRef();
   }
 
   state = {
@@ -14,53 +15,57 @@ class AudioPlayer extends React.Component {
   }
 
   componentDidMount() {
+    this.audioRef.current.seekTo(this.props.audioCurrentPlayTime, 'seconds');
     if (sessionStorage.getItem('isNotPlaying') === 'true') return this.setState({ pause: '', autoplay: null });
     else if (sessionStorage.getItem('isNotPlaying') === 'false') return this.setState({ pause: 'paused', autoplay: 'play' });
+    else if (this.state.autoplay) this.setState({ pause: 'paused' });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.currentAudioTime);
   }
 
   songEnd = () => {
     this.setState({ pause: '' });
   };
 
-  handlePlayPauseClick = event => {
+  handlePlayPauseClick = () => {
     if (this.state.pause) {
+      // song was playing
       this.setState({ pause: '' });
-      event.target.pauseVideo();
     }
     else {
       this.setState({ pause: 'paused' });
-      event.target.playVideo();
     }
     sessionStorage.setItem(`isNotPlaying`, `${!!this.state.pause}`);
   };
 
-  handleTimeUpdate = () => {
-    this.props.changeAudioCurrentPlayTime(this.audioRef.current.currentTime);
+  handleTimeUpdate = (duration) => {
+    console.log(duration);
+    this.props.changeAudioCurrentPlayTime(duration);
+  }
+
+  handleOnStart = () => {
+    this.currentAudioTime = setInterval(() => this.props.changeAudioCurrentPlayTime(this.audioRef.current.getCurrentTime()), 1000);
   }
 
   render() {
-    const iFrameParameters = {
-      height: '0',
-      width: '0',
-      playerVars: { autoplay: 1 },
-    }
-
     return (
       <div className={this.props.className}>
-
         <div
           className={`button ${this.state.pause}`}
           onClick={this.handlePlayPauseClick}
         >
-        </div>
-        <YouTube 
-          videoId={this.videoId}
-          opts={iFrameParameters}
-          onPlay={this.handlePlayPauseClick}
-          onPause={this.handlePlayPauseClick}
-          onEnd={this.onEnd}
-          onReady={(event) => event.target.playVideo()}
+        <ReactPlayer
+          ref={this.audioRef}
+          url={this.props.audioPreviewURL}
+          playing={!!this.state.pause}
+          onStart={this.handleOnStart}
+          onEnded={this.songEnd}
+          width={0}
+          height={0}
         />
+        </div>
       </div>
     );
   }
