0d3b35c22cf2cc3918c3ec27c0c11a14da2f5d20
Bibek Saha <thebibeksaha@gmail.com>
2020-08-20 04:36:11 +0530

    made some changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,10 +12,10 @@ const About = () => {
       <img id="bibek-saha-image" alt="bibek saha" src={bibekSahaImage} />
       <div>
         Hi! I am <strong className="make-it-yellow">Bibek Saha</strong> and I'm the creator of Hover in Harmony<br /><br />
-        Hover in Harmony is an FREE service where one can listen to the music and read the lyrics at the same time.<br/><br/>
-        <em style={{fontSize: '0.9rem'}}>Disclaimer: The rights to the song and lyrics goes to the individual artist and the record label</em><br/><br/>
+        Hover in Harmony is an FREE service where one can listen to the music and read the lyrics at the same time<br/><br/>
         This is definitely one of the complicated project that I undertook. It has been an absolute joy working on this<br /><br />
-        In future if I will try to add more features. If you find any bug do ping me on social media, the links are given below. I will try to patch it.
+        In future if I will try to add more features. If you find any bug do ping me on social media, the links are given below. I will try to patch it<br/><br/>
+        <em style={{fontSize: '0.9rem', color: 'var(--very-muted)'}}>Disclaimer: The rights to the song and lyrics goes to the individual artist and the record label</em>
         <Footer />
       </div>
     </div>
