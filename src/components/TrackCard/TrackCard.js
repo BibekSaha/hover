037e8746caa4c000172585c01fce18a5474664c0
Bibek Saha <thebibeksaha@gmail.com>
2020-10-08 02:25:46 +0530

    fixed a typo that was crashing the app

--- src/components/TrackCard/TrackCard.js
+++ src/components/TrackCard/TrackCard.js
@@ -6,7 +6,7 @@ import './TrackCard.css';
 
 class TrackCard extends React.Component {
   render() {
-    if (!this.props.userTitle) return;
+    if (!this.props.userTitle) return null;
     const link = `/search/${this.props.userTitle.split(' ').join('-')}`;
     return (
       <>
