6092c8d686b77b829edc9a23ab253189f4f5b866
Bibek Saha <thebibeksaha@gmail.com>
2020-10-10 16:15:21 +0530

    changed the api

--- functions/search.js
+++ functions/search.js
@@ -1,76 +1,89 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const rp = require("request-promise");
+const $ = require("cheerio");
 
-const GENIUS_BASE_URI = 'https://api.genius.com';
-const GENIUS_BASE_SEARCH_URI = GENIUS_BASE_URI + `/search?access_token=${process.env.REACT_APP_GENIUS_TOKEN}&q=`;
-const LYRICS_BASE_URL = `https://api.lyrics.ovh/v1`;
-const GENIUS_AUTH_HEADERS = {
-  Authorization: `Bearer ${process.env.REACT_APP_GENIUS_TOKEN}`
+const fetchLyrics = url => {
+  return rp(url)
+    .then(html => JSON.stringify({ lyrics: $(".lyrics", html).text().trim() }))
 };
 
-let GENIUS_SONG_DETAILS_URI;
+const GENIUS_BASE_URI = "https://api.genius.com";
+const GENIUS_BASE_SEARCH_URI =
+  GENIUS_BASE_URI +
+  `/search?access_token=${process.env.REACT_APP_GENIUS_TOKEN}&q=`;
+const GENIUS_AUTH_HEADERS = {
+  Authorization: `Bearer ${process.env.REACT_APP_GENIUS_TOKEN}`,
+};
+
+let GENIUS_SONG_DETAILS_URI, GENIUS_LYRICS_URI;
 
 const search = (song) => {
   const GENIUS_SEARCH_URI = GENIUS_BASE_SEARCH_URI + encodeURIComponent(song);
   const cache = {
-    fullTitle: '',
-    artistName: '',
-    imageURL: '',
-    audioPreviewURL: '',
-    lyrics: '',
+    fullTitle: "",
+    artistName: "",
+    imageURL: "",
+    audioPreviewURL: "",
+    lyrics: "",
   };
 
   return fetch(GENIUS_SEARCH_URI, GENIUS_AUTH_HEADERS)
-    .then(resp => resp.json())
-    .then(resp => resp.response.hits[0].result)
-    .then(songDataResp => {
+    .then((resp) => resp.json())
+    .then((resp) => resp.response.hits[0].result)
+    .then((songDataResp) => {
       cache.fullTitle = songDataResp.title;
       cache.artistName = songDataResp.primary_artist.name;
       cache.imageURL = songDataResp.song_art_image_thumbnail_url;
+      GENIUS_LYRICS_URI = songDataResp.url;
 
       const songAPIpath = songDataResp.api_path;
-      GENIUS_SONG_DETAILS_URI = GENIUS_BASE_URI + `${songAPIpath}?access_token=${process.env.REACT_APP_GENIUS_TOKEN}&text_format=plain`;
+      GENIUS_SONG_DETAILS_URI =
+        GENIUS_BASE_URI +
+        `${songAPIpath}?access_token=${process.env.REACT_APP_GENIUS_TOKEN}&text_format=plain`;
 
       return fetch(GENIUS_SONG_DETAILS_URI, GENIUS_AUTH_HEADERS);
     })
-    .then(resp => resp.json())
-    .then(resp => resp.response.song.media)
-    .then(songMediaData => {
-      const youTubeObject = songMediaData.find(mediaObj => mediaObj.provider === 'youtube');
+    .then((resp) => resp.json())
+    .then((resp) => resp.response.song.media)
+    .then((songMediaData) => {
+      const youTubeObject = songMediaData.find(
+        (mediaObj) => mediaObj.provider === "youtube"
+      );
       cache.audioPreviewURL = youTubeObject.url;
 
       // sanitizing the title and the artist name
-      const artist = encodeURIComponent(cache.artistName.split('&')[0].trim());
+      const artist = encodeURIComponent(cache.artistName.split("&")[0].trim());
       const title = encodeURIComponent(cache.fullTitle.trim());
-      return fetch(`${LYRICS_BASE_URL}/${artist}/${title}`);
+      return fetchLyrics(GENIUS_LYRICS_URI);
     })
-    .then(resp => resp.text())
-    .then(resp => JSON.parse(resp))
-    .then(resp => {
-      if (resp.error || !resp.lyrics) {
-        if (!cache.audioPreviewURL) return Promise.reject({errorMessage: 'Not Found'});
-        cache.lyrics = '';
-      } else {
+    .then((resp) => JSON.parse(resp))
+    .then((resp) => {
+      // if (resp.error || !resp.lyrics) {
+      //   if (!cache.audioPreviewURL)
+      //     return Promise.reject({ errorMessage: "Not Found" });
+      //   cache.lyrics = "";
+      // } else {
         cache.lyrics = resp.lyrics;
-      }
+      // }
       return cache;
     })
-    .catch(() => Promise.reject({errorMessage: 'Not Found'}));
+    .catch(() => Promise.reject({ errorMessage: "Not Found" }));
 };
 
 exports.handler = async function (event, context) {
-  if (event.httpMethod === 'GET') {
+  if (event.httpMethod === "GET") {
     const songUserTitle = event.queryStringParameters.q;
     try {
       const response = await search(songUserTitle);
       return {
         statusCode: 200,
-        body: JSON.stringify(response)
+        body: JSON.stringify(response),
       };
     } catch (err) {
       return {
         statusCode: 404,
-        body: JSON.stringify(err)
+        body: JSON.stringify(err),
       };
     }
   }
-}
+};
